117a44ed672e485cff7b23a640f5d968be855638
ravinayag <52193049+ravinayag@users.noreply.github.com>
2019-06-27 06:18:05 +0000

    Create download_hlf.sh

---
+++ download_hlf.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+curl -sSL   https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s
+### Short link: uncomment this if have to do with short link
+#curl -sSL http://bit.ly/2ysbOFE | bash -s
+chmod 777 -R fabric-samples
+
+
+#### if you need to download specific version follow below syntax
+curl -sSL http://bit.ly/2ysbOFE | bash -s -- <fabric_version> <fabric-ca_version> <thirdparty_version>
+curl -sSL http://bit.ly/2ysbOFE | bash -s -- 1.4.1 1.4.1 0.4.15
