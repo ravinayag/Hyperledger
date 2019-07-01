40edb2a65f2b91332e99cf1b24eb89bb9b82d49e
ravinayag <52193049+ravinayag@users.noreply.github.com>
2019-07-01 17:14:39 +0000

    Create composer.sh

---
+++ composer.sh
@@ -0,0 +1,13 @@
+## This document prepared based on the version hlf-1.4, composer*@20
+
+#npm version 6.9
+#node version 8.16.1
+#composer-cli version v0.20
+#composer-rest-server version 0.20
+#generator-hyperledger-composer version 0.20
+#yo version 2.05
+
+
+npm install -g composer-cli composer-rest-server generator-hyperledger-composer yo
+npm install -g composer-playground
+
